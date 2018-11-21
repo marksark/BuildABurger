14cd2e08a4f674a3f5a2379133f052dcadc805cb
Mark Sarkis <mark.sarkis@hitachiconsulting.com>
2018-11-21 15:16:12 -0600

    layout established

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+  render() {
+    return(
+      <Auxiliary>
+        <div>Burger Graph</div>
+        <div>Build Control Div</div>
+      </Auxiliary>
+    );
+  }
+}
+
+export default BurgerBuilder;
