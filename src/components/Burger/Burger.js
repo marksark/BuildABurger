e432cad4f2e2dc5966ad9f4d7eea0afe35dd5d7c
Mark Sarkis <mark.sarkis93@gmail.com>
2019-04-19 17:02:59 -0500

    ejected react app, updated components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredients from './BurgerIngredients/BurgerIngredients';
 
-class Burger extends Component {
-    render() {
-        return(
-            <div className={classes.Burger}>
-                <BurgerIngredients type="bread-top"/>    
-                <BurgerIngredients type="cheese"/>    
-                <BurgerIngredients type="meat"/>    
-                <BurgerIngredients type="bread-bottom"/>    
-            </div>
-        );
-    }
+const burger = (props) => {
+    const incomingIngredients = Object.keys(props.ingredients)
+    //transform incoming ingredients object into an array
+    .map(each => {
+        //capture the length of an array that matches # of each ingredient (i.e. double meat, single cheese)
+        return [...Array(props.ingredients[each])].map((_,i) => {
+            return <BurgerIngredients key={each+i} type={each}/>;
+        });
+    });
+    console.log(incomingIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredients type="bread-top"/>    
+            {incomingIngredients}   
+            <BurgerIngredients type="bread-bottom"/>    
+        </div>
+    );
 }
 
-export default Burger;
+export default burger;
